849c00a13acab6bf06b24d437a32eaa1440166f7
Your Name <you@example.com>
2026-03-09 09:05:36 -0300

    Organize Imports

--- src/main/java/br/com/senai/api_vendas/entity/Categorias.java
+++ src/main/java/br/com/senai/api_vendas/entity/Categorias.java
@@ -2,7 +2,6 @@ package br.com.senai.api_vendas.entity;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
--- src/main/java/br/com/senai/api_vendas/entity/Pedido.java
+++ src/main/java/br/com/senai/api_vendas/entity/Pedido.java
@@ -14,9 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
 
 @Entity
